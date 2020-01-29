1ce08345af7862eaedc5425ff0992c3cdbac6b6f
Timur <timur.rasulev.82@gmail.com>
2020-01-29 11:41:23 +0300

    Test

--- src/index.js
+++ src/index.js
@@ -12,4 +12,4 @@ SortDedupCollection.of = function(...elements) {
   return this;
 };
 
-console.log(SortDedupCollection.of(1, 2, 2, 3, 3, 4, 5, 5, 5));
+console.log(SortDedupCollection.of(1, 2, 2, 3, 3, 4, 5, 5, 5).toString());
